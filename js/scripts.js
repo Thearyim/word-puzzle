3e6864005319f26c983229793692da04f73e74ec
Theary Im and Yulia Shidlovskaya <yulia@>
2019-01-23 10:41:49 -0800

    separate business and user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,27 @@
 $(document).ready(function(){
+  var vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
+
+  var replaceVowel = function(char) {
+    for (var vowelsIndex = 0; vowelsIndex < vowels.length; vowelsIndex ++) {
+      if (char === vowels[vowelsIndex]) {
+        char = "_";
+      }
+    }
+    return char;
+  }
+
   $("#formOne").submit(function(event) {
 
     var inputText = $("input#text").val();
     var outputText = "";
-    var vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
 
     for (var index = 0; index < inputText.length; index ++) {
       var inputChar = inputText[index];
-      for (var vowelsIndex = 0; vowelsIndex < vowels.length; vowelsIndex ++) {
-        if (inputChar === vowels[vowelsIndex]) {
-          inputChar = "_";
-        }
-      }
-      outputText = outputText + inputChar;
+      var replaced = replaceVowel(inputChar);
+
+      outputText = outputText + replaced;
     }
     $("#result").text(outputText);
-
-
 
     event.preventDefault();
 
